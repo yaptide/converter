a12dca3054a9480fe259b36dfe7c3b3fafbd8ce5
retkiewi <krzret@student.agh.edu.pl>
2021-10-30 23:14:49 +0200

    Refactors in test_dummy_converter

--- tests/test_dummy_converter.py
+++ tests/test_dummy_converter.py
@@ -3,7 +3,7 @@ import tempfile
 from os import path
 from converter.converter import BeamConfig, MatConfig, DetectConfig, GeoConfig, DummmyParser, Runner
 
-BEAM_STR = """
+_Beam_str = """
 RNDSEED      	89736501     ! Random seed
 JPART0       	2            ! Incident particle type
 TMAX0      	250.000000   0.0  ! Incident energy; (MeV/nucl)
@@ -12,12 +12,12 @@ STRAGG          2            ! Straggling: 0-Off 1-Gauss, 2-Vavilov
 MSCAT           2            ! Mult. scatt 0-Off 1-Gauss, 2-Moliere
 NUCRE           0            ! Nucl.Reac. switcher: 1-ON, 0-OFF
 """
-MAT_STR = """MEDIUM 0
+_Mat_str = """MEDIUM 0
 ICRU 276
 END
 """
 
-DETECT_STR = """Geometry Cyl
+_Detect_str = """Geometry Cyl
     Name ScoringCylinder
     R 0.0 10.0 10
     Z 0.0 30.0 10
@@ -32,7 +32,7 @@ Output
     Quantity Dose
     """
 
-GEO_STR = """
+_Geo_str = """
 *---><---><--------><------------------------------------------------>
     0    0           protons, H2O 30 cm cylinder, r=10, 1 zone
 *---><---><--------><--------><--------><--------><--------><-------->
@@ -52,54 +52,56 @@ GEO_STR = """
     1 1000    0
 """
 
-TEST_DIR = tempfile.TemporaryDirectory()
+_Test_dir = './test_runs'
+
+
+@pytest.fixture
+def converter_output_dir(tmp_path_factory):
+    output_dir = tmp_path_factory.mktemp(_Test_dir)
+    dummy_runner = Runner(DummmyParser(), {}, output_dir)
+    dummy_runner.run_parser()
+    return output_dir
 
 
 def test_default_beam_config_str() -> None:
     """Test if the default BeamConfig str representation is correct."""
-    assert str(BeamConfig()) == BEAM_STR
+    assert str(BeamConfig()) == _Beam_str
 
 
 def test_default_mat_config_str() -> None:
     """Test if the default MatConfig str representation is correct."""
-    assert str(MatConfig()) == MAT_STR
+    assert str(MatConfig()) == _Mat_str
 
 
 def test_default_detect_config_str() -> None:
     """Test if the default MatConfig str representation is correct."""
-    assert str(DetectConfig()) == DETECT_STR
+    assert str(DetectConfig()) == _Detect_str
 
 
 def test_default_geo_config_str() -> None:
     """Test if the default MatConfig str representation is correct."""
-    assert str(GeoConfig()) == GEO_STR
+    assert str(GeoConfig()) == _Geo_str
 
 
-def test_parser_run() -> None:
-    """Test if dummy parser generates output files corectly"""
-    dummy_runner = Runner(DummmyParser(), {}, TEST_DIR.name)
-    dummy_runner.run_parser()
-
-
-def test_if_beam_created() -> None:
+def test_if_beam_created(converter_output_dir) -> None:
     """Check if beam.dat file created"""
-    with open(path.join(TEST_DIR.name, 'beam.dat')) as f:
-        assert f.read() == BEAM_STR
+    with open(path.join(converter_output_dir, 'beam.dat')) as f:
+        assert f.read() == _Beam_str
 
 
-def test_if_mat_created() -> None:
+def test_if_mat_created(converter_output_dir) -> None:
     """Check if mat.dat file created"""
-    with open(path.join(TEST_DIR.name, 'mat.dat')) as f:
-        assert f.read() == MAT_STR
+    with open(path.join(converter_output_dir, 'mat.dat')) as f:
+        assert f.read() == _Mat_str
 
 
-def test_if_detect_created() -> None:
+def test_if_detect_created(converter_output_dir) -> None:
     """Check if detect.dat file created"""
-    with open(path.join(TEST_DIR.name, 'detect.dat')) as f:
-        assert f.read() == DETECT_STR
+    with open(path.join(converter_output_dir, 'detect.dat')) as f:
+        assert f.read() == _Detect_str
 
 
-def test_if_geo_created() -> None:
+def test_if_geo_created(converter_output_dir) -> None:
     """Check if geo.dat file created"""
-    with open(path.join(TEST_DIR.name, 'geo.dat')) as f:
-        assert f.read() == GEO_STR
+    with open(path.join(converter_output_dir, 'geo.dat')) as f:
+        assert f.read() == _Geo_str
