e37c3c4569f338a59be074ddf36389be9dcb0a9b
retkiewi <krzret@student.agh.edu.pl>
2021-12-14 00:20:41 +0100

    Added beampos

--- converter/shieldhit/beam.py
+++ converter/shieldhit/beam.py
@@ -7,6 +7,7 @@ class BeamConfig:
 
     energy: float = 150.
     nstat: int = 10000
+    beampos: tuple[float, float, float] = (0, 0, 0)
 
     beam_template: str = """
 RNDSEED      	89736501     ! Random seed
@@ -16,7 +17,11 @@ NSTAT       {nstat:d}    0       ! NSTAT, Step of saving
 STRAGG          2            ! Straggling: 0-Off 1-Gauss, 2-Vavilov
 MSCAT           2            ! Mult. scatt 0-Off 1-Gauss, 2-Moliere
 NUCRE           1            ! Nucl.Reac. switcher: 1-ON, 0-OFF
+BEAMPOS {pos_x} {pos_y} {pos_z} ! Position of the beam
 """
 
     def __str__(self) -> str:
-        return self.beam_template.format(energy=self.energy, nstat=self.nstat)
+        return self.beam_template.format(
+            energy=self.energy, nstat=self.nstat,
+            pos_x=self.beampos[0], pos_y=self.beampos[1], pos_z=self.beampos[2]
+        )
