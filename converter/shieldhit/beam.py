74d68f9c31c8ef347f37cb9ec15b3c70cc626c93
Leszek Grzanka <grzanka@agh.edu.pl>
2022-09-13 19:22:12 +0000

    improved beam config

--- converter/shieldhit/beam.py
+++ converter/shieldhit/beam.py
@@ -7,6 +7,9 @@ class BeamConfig:
     """Class mapping of the beam.dat config file."""
 
     energy: float = 150.
+    energy_spread: float = 1.5
+    beam_ext_x: float = -0.1
+    beam_ext_y: float = 0.1
     nstat: int = 10000
     beampos: tuple[float, float, float] = (0, 0, 0)
     beamdir: tuple[float, float, float] = (0, 0, 1)
@@ -14,13 +17,15 @@ class BeamConfig:
     beam_template: str = """
 RNDSEED      	89736501     ! Random seed
 JPART0       	2            ! Incident particle type
-TMAX0      	{energy:3.1f}  1.5       ! Incident energy; (MeV/nucl)
+TMAX0      	{energy}  {energy_spread}       ! Incident energy; (MeV/nucl)
 NSTAT       {nstat:d}    0       ! NSTAT, Step of saving
 STRAGG          2            ! Straggling: 0-Off 1-Gauss, 2-Vavilov
 MSCAT           2            ! Mult. scatt 0-Off 1-Gauss, 2-Moliere
 NUCRE           1            ! Nucl.Reac. switcher: 1-ON, 0-OFF
 BEAMPOS {pos_x} {pos_y} {pos_z} ! Position of the beam
 BEAMDIR {theta} {phi} ! Direction of the beam
+BEAMSIGMA  {beam_ext_x} {beam_ext_y}  ! Beam extension
+DELTAE   {delta_e}   ! relative mean energy loss per transportation step
 """
 
     @staticmethod
@@ -45,10 +50,13 @@ BEAMDIR {theta} {phi} ! Direction of the beam
         theta, phi, _ = BeamConfig.cartesian2spherical(self.beamdir)
         return self.beam_template.format(
             energy=self.energy,
+            energy_spread=self.energy_spread,
             nstat=self.nstat,
             pos_x=self.beampos[0],
             pos_y=self.beampos[1],
             pos_z=self.beampos[2],
+            beam_ext_x=self.beam_ext_x,
+            beam_ext_y=self.beam_ext_y,
             theta=theta,
             phi=phi,
         )
