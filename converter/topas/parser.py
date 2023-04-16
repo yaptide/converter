50bc15d46cc5caa49550a3a67c6945ce13d7d511
Mateusz Wronka <matwro2001@gmail.com>
2023-04-16 13:58:38 +0200

    added beam to dummy config file

--- converter/topas/parser.py
+++ converter/topas/parser.py
@@ -18,6 +18,33 @@ d:Ge/MyBox/RotX     = 0. deg
 d:Ge/MyBox/RotY     = 0. deg
 d:Ge/MyBox/RotZ     = 0. deg
 
+s:So/Demo/Type = "Beam" 
+s:So/Demo/Component = "BeamPosition"
+s:So/Demo/BeamParticle = "proton"
+d:So/Demo/BeamEnergy = 169.23 MeV
+u:So/Demo/BeamEnergySpread = 0.757504
+s:So/Demo/BeamPositionDistribution = "Gaussian" 
+s:So/Demo/BeamPositionCutoffShape = "Ellipse" 
+d:So/Demo/BeamPositionCutoffX = 10. cm 
+d:So/Demo/BeamPositionCutoffY = 10. cm 
+d:So/Demo/BeamPositionSpreadX = 0.65 cm 
+d:So/Demo/BeamPositionSpreadY = 0.65 cm 
+s:So/Demo/BeamAngularDistribution = "Gaussian" 
+d:So/Demo/BeamAngularCutoffX = 90. deg
+d:So/Demo/BeamAngularCutoffY = 90. deg
+d:So/Demo/BeamAngularSpreadX = 0.0032 rad 
+d:So/Demo/BeamAngularSpreadY = 0.0032 rad 
+i:So/Demo/NumberOfHistoriesInRun = 0
+
+s:Ge/BeamPosition/Parent="World"
+s:Ge/BeamPosition/Type="Group"
+d:Ge/BeamPosition/TransX=0. m
+d:Ge/BeamPosition/TransY=0. m
+d:Ge/BeamPosition/TransZ= Ge/World/HLZ m
+d:Ge/BeamPosition/RotX=180. deg
+d:Ge/BeamPosition/RotY=0. deg
+d:Ge/BeamPosition/RotZ=0. deg
+
 sv:Ph/Default/Modules = 1 "g4em-standard_opt0"
 """
 
