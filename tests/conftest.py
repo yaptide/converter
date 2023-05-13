73ab7d365eb2deb3cf78a249d72f46a61c687450
Leszek Grzanka <grzanka@agh.edu.pl>
2023-05-13 21:49:53 +0200

    fixing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,14 @@ import pytest
 import json
 
 @pytest.fixture(scope='session')
-def project_shieldhit_json() -> dict:
-    """Dictionary with project data for SHIELD-HIT12A"""
+def project_shieldhit_path() -> Path:
+    """Path to SHIELD-HIT12A project.json file"""
     this_file_location = Path(__file__).parent
     json_file_path = this_file_location / 'shieldhit' / 'resources' / 'project.json'
-    with open(json_file_path, 'r') as file_handle:
+    return json_file_path
+
+@pytest.fixture(scope='session')
+def project_shieldhit_json(project_shieldhit_path) -> dict:
+    """Dictionary with project data for SHIELD-HIT12A"""
+    with open(project_shieldhit_path, 'r') as file_handle:
         return json.load(file_handle)
--- tests/shieldhit/test_shieldhit_converter.py
+++ tests/shieldhit/test_shieldhit_converter.py
@@ -9,30 +9,33 @@ from converter.api import get_parser_from_str, run_parser
 from converter.shieldhit.parser import Parser
 
 
-@pytest.fixture
-def parser() -> Parser:
-    """SHIELD-HIT12A parser fixture"""
-    return get_parser_from_str('shieldhit')
+def test_parser(sh12a_parser : Parser) -> None:
+    """Check if parser is created correctly"""
+    assert sh12a_parser
+    assert sh12a_parser.info['version'] == ''
+    assert sh12a_parser.info['simulator'] == 'shieldhit'
 
+def test_project_json(project_shieldhit_json: dict) -> None:
+    """Check if project json is created correctly"""
+    assert project_shieldhit_json
+    assert "metadata" in project_shieldhit_json
+    assert "project" in project_shieldhit_json
+    assert "scene" in project_shieldhit_json
+    assert "physic" in project_shieldhit_json
+    assert "energyLoss" in project_shieldhit_json["physic"]
+    assert "enableNuclearReactions" in project_shieldhit_json["physic"]
+    assert "energyModelStraggling" in project_shieldhit_json["physic"]
+    assert "multipleScattering" in project_shieldhit_json["physic"]
 
-
-
-def test_if_parser_created(parser: Parser) -> None:
-    """Check if parser is created"""
-    assert parser
-    assert parser.info['version'] == ''
-    assert parser.info['label'] == ''
-    assert parser.info['simulator'] == 'shieldhit'
 
 @pytest.mark.parametrize('filename', ['beam.dat', 'mat.dat', 'detect.dat'])
-def test_if_expected_files_created(parser: Parser, default_json : dict, tmp_path : Path, filename: str) -> None:
+def test_if_expected_files_created(sh12a_parser: Parser, project_shieldhit_json : dict, tmp_path : Path, path_to_dir_with_expected_output: Path, filename: str) -> None:
     """Check if all output files are created"""
     logging.info('Checking %s file', filename)
-    run_parser(parser, default_json, tmp_path)
-    dir_with_expected_files = Path(__file__).parent.parent / 'input_examples' / 'expected_shieldhit_output'
+    run_parser(sh12a_parser, project_shieldhit_json, tmp_path)
     assert (tmp_path / filename).exists()
-    assert (dir_with_expected_files / filename).exists()
-    assert filecmp.cmp(tmp_path / filename, dir_with_expected_files / filename)
+    assert (path_to_dir_with_expected_output / filename).exists()
+    assert filecmp.cmp(tmp_path / filename, path_to_dir_with_expected_output / filename)
 
 @pytest.mark.parametrize('filename', ['geo.dat'])
 @pytest.mark.skip(reason="Something is wrong with geo.dat file.")
