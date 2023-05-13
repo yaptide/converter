85605e815426d728d1b47772d7858d65bb6ca9c4
Leszek Grzanka <grzanka@agh.edu.pl>
2023-05-13 22:27:10 +0200

    deepsource

--- tests/shieldhit/test_zones.py
+++ tests/shieldhit/test_zones.py
@@ -7,6 +7,7 @@ def water_phantom_zone_dict(project_shieldhit_json):
     return project_shieldhit_json['zoneManager']['zones'][0]
 
 def test_water_phantom_zone(water_phantom_zone_dict):
+    """Test if the water phantom zone is parsed correctly"""
     assert water_phantom_zone_dict['name'] == 'Water_phantom_zone'
     zone_obj = Zone(water_phantom_zone_dict)
     assert zone_obj.id == 1
