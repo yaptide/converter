c73c39503ab844dc06df286235102a6fe7448346
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-07-04 06:43:47 +0200

    Add missing test

--- tests/shieldhit/test_shieldhit_converter.py
+++ tests/shieldhit/test_shieldhit_converter.py
@@ -16,6 +16,7 @@ def test_parser(sh12a_parser : Parser) -> None:
     assert sh12a_parser.info['simulator'] == 'shieldhit'
     assert sh12a_parser.info['label'] == ''
 
+
 def test_project_json(project_shieldhit_json: dict) -> None:
     """Check if project json is created correctly"""
     assert project_shieldhit_json
@@ -46,6 +47,26 @@ def test_if_expected_files_created(sh12a_parser: Parser, project_shieldhit_json 
             difference_lines = "\n".join(differ.compare(generated_f.readlines(), expected_f.readlines()))
             logging.info("Difference between files: %s", difference_lines)
     assert expected_equal_to_generated
+
+
+@pytest.mark.parametrize('filename', ['beam.dat', 'mat.dat', 'detect.dat', 'sobp.dat'])
+def test_if_expected_files_created_with_sobp_dat(sh12a_parser: Parser, project_shieldhit_json_with_sobp_dat : dict, tmp_path : Path, path_to_dir_with_expected_output_with_sobp_dat: Path, filename: str) -> None:
+    """Check if all output files are created"""
+    logging.info('Checking %s file', filename)
+    run_parser(sh12a_parser, project_shieldhit_json_with_sobp_dat, tmp_path)
+    assert (tmp_path / filename).exists()
+    assert (path_to_dir_with_expected_output_with_sobp_dat / filename).exists()
+    expected_equal_to_generated = filecmp.cmp(tmp_path / filename, path_to_dir_with_expected_output_with_sobp_dat / filename)
+    if not expected_equal_to_generated:
+        logging.info("Expected file at %s", path_to_dir_with_expected_output_with_sobp_dat / filename)
+        logging.info("Generated file at %s", tmp_path / filename)
+        logging.info("Difference between files:")
+        with open(tmp_path / filename) as generated_f, open(path_to_dir_with_expected_output_with_sobp_dat / filename) as expected_f:
+            differ = Differ()
+            difference_lines = "\n".join(differ.compare(generated_f.readlines(), expected_f.readlines()))
+            logging.info("Difference between files: %s", difference_lines)
+    assert expected_equal_to_generated
+
 
 @pytest.mark.parametrize('filename', ['geo.dat'])
 @pytest.mark.skip(reason="Something is wrong with geo.dat file.")
