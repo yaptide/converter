46288eaa31ea236b58ec5fb3a3a84b7d128e56f3
Mateusz Wronka <matwro2001@gmail.com>
2023-10-15 15:48:52 +0200

    improve fluka converter tests

--- tests/fluka/conftest.py
+++ tests/fluka/conftest.py
@@ -1,6 +1,12 @@
 import pytest
+from pathlib import Path
 
 @pytest.fixture(scope='session')
 def project_fluka_json(project_shieldhit_json) -> dict:
     """We do not have yet project.json for FLUKA, so we use the one from SHIELD-HIT12A"""
     return project_shieldhit_json
+
+@pytest.fixture(scope='session')
+def expected_output_path() -> Path:
+    """Just a parser fixture."""
+    return Path(__file__).parent / 'expected_fluka_output'
