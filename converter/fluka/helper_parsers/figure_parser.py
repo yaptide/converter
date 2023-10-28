b80ddb68244ac137b3dfc299392e309347b0f8d1
Mateusz Wronka <matwro2001@gmail.com>
2023-10-28 12:21:17 +0200

    figure unit tests

--- converter/fluka/helper_parsers/figure_parser.py
+++ converter/fluka/helper_parsers/figure_parser.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from converter import solid_figures
 from converter.solid_figures import BoxFigure, CylinderFigure, SolidFigure, SphereFigure
 from scipy.spatial.transform import Rotation as R
@@ -12,38 +12,38 @@ class FlukaFigure:
 @dataclass(frozen=False)
 class FlukaBox(FlukaFigure):
     figure_type: str = "BOX"
-    coordinates: list[float] = [0, 0, 0]
-    x_vector: list[float] = [0, 0, 0]
-    y_vector: list[float] = [0, 0, 0]
-    z_vector: list[float] = [0, 0, 0]
+    coordinates: list[float] = field(default_factory=lambda: (0, 0, 0))
+    x_vector: list[float] = field(default_factory=lambda: (0, 0, 0))
+    y_vector: list[float] = field(default_factory=lambda: (0, 0, 0))
+    z_vector: list[float] = field(default_factory=lambda: (0, 0, 0))
 
 @dataclass(frozen=False)
 class FlukaCylinder(FlukaFigure):
     figure_type: str = "RCC"
-    coordinates: list[float] = [0, 0, 0]
-    height_vector: list[float] = [0, 0, 0]
+    coordinates: list[float] = field(default_factory=lambda: (0, 0, 0))
+    height_vector: list[float] = field(default_factory=lambda: (0, 0, 0))
     radius: float = 0
 
 @dataclass(frozen=False)
 class FlukaSphere(FlukaFigure):
     figure_type: str = "SPH"
-    coordinates: list[float] = [0, 0, 0]
+    coordinates: list[float] = field(default_factory=lambda: (0, 0, 0))
     radius: float = 0
 
 def parse_box(box: BoxFigure) -> FlukaBox:
     """Parse box to Fluka box"""
     rotation = R.from_euler('xyz', box.rotation, degrees=True)
-    x_vec = rotation.apply([box.x_edge_length, 0, 0])
-    y_vec = rotation.apply([0, box.y_edge_length, 0])
-    z_vec = rotation.apply([0, 0, box.z_edge_length])
+    x_vec = tuple(rotation.apply((box.x_edge_length, 0, 0)))
+    y_vec = tuple(rotation.apply((0, box.y_edge_length, 0)))
+    z_vec = tuple(rotation.apply((0, 0, box.z_edge_length)))
     diagonal_vec = x_vec + y_vec + z_vec
 
     fluka_box = FlukaBox()
-    fluka_box.coordinates = [
+    fluka_box.coordinates = (
         box.position[0] - diagonal_vec[0] / 2,
         box.position[1] - diagonal_vec[1] / 2,
         box.position[2] - diagonal_vec[2] / 2,
-    ]
+    )
     fluka_box.x_vector = x_vec
     fluka_box.y_vector = y_vec
     fluka_box.z_vector = z_vec
@@ -54,14 +54,14 @@ def parse_box(box: BoxFigure) -> FlukaBox:
 def parse_cylinder(cylinder: CylinderFigure) -> FlukaCylinder:
     """Parse cylinder to Fluka cylinder"""
     rotation = R.from_euler('xyz', cylinder.rotation, degrees=True)
-    height_vector = rotation.apply([0, 0, cylinder.height])
+    height_vector = rotation.apply((0, 0, cylinder.height))
 
     fluka_cylinder = FlukaCylinder()
-    fluka_cylinder.coordinates= [
+    fluka_cylinder.coordinates= (
         cylinder.position[0] - height_vector[0] / 2,
         cylinder.position[1] - height_vector[1] / 2,
         cylinder.position[2] - height_vector[2] / 2,
-    ]
+    )
     fluka_cylinder.height_vector = height_vector
     fluka_cylinder.radius = cylinder.radius_top
     fluka_cylinder.uuid = cylinder.uuid
@@ -72,7 +72,9 @@ def parse_sphere(sphere: SphereFigure) -> FlukaSphere:
     """Parse sphere to Fluka sphere"""
     fluka_sphere = FlukaSphere()
     fluka_sphere.radius = sphere.radius
-    fluka_sphere.coordinates = [coord for coord in sphere.position]
+    fluka_sphere.coordinates = (sphere.position[0],
+                                sphere.position[1],
+                                sphere.position[2])
     fluka_sphere.uuid = sphere.uuid
     return fluka_sphere
 
--- tests/fluka/test_figure_parser.py
+++ tests/fluka/test_figure_parser.py
@@ -1,26 +1,10 @@
 import logging
+import math
 from converter import solid_figures
 from converter.fluka.helper_parsers.figure_parser import FlukaCylinder, FlukaBox, FlukaSphere, parse_box, parse_cylinder, parse_sphere
 import pytest
-from tests.fluka.conftest import project_fluka_json
 
-@pytest.fixture(scope='module')
-def box_dict(project_fluka_json):
-    """Part of the project.json file with the box figure"""
-    return project_fluka_json['figureManager']['figures'][0]
-
-@pytest.fixture(scope='module')
-def cylinder_dict(project_fluka_json):
-    """Part of the project.json file with the cylinder figure"""
-    return project_fluka_json['figureManager']['figures'][1]
-
-@pytest.fixture(scope='module')
-def sphere_dict(project_fluka_json):
-    """Part of the project.json file with the sphere figure"""
-    return project_fluka_json['figureManager']['figures'][2]
-
-
-def test_parse_cylinder(cylinder_dict):
+def test_parse_cylinder():
     cylinder = solid_figures.CylinderFigure(uuid='ed1507a5-0489-4dc3-bb87-7b27dcff43e7',
                                           name='Cylinder',
                                           position=(2.14, 1.32, 0),
@@ -28,23 +12,30 @@ def test_parse_cylinder(cylinder_dict):
                                           radius_top=1,
                                           radius_bottom=1,
                                           height=1)
-    
-    expected_fluka_cylinder = FlukaCylinder(figure_type = "RCC",
-                                            name = "Cylinder",
-                                            uuid = "ed1507a5-0489-4dc3-bb87-7b27dcff43e7",
-                                            coordinates = [0, 0, 0],
-                                            height_vector = [0, 0, 0],
-                                            radius = 0)
-    
 
-    assert parse_cylinder(cylinder) == expected_fluka_cylinder
+    fluka_cylinder = parse_cylinder(cylinder)
+
+    assert fluka_cylinder.figure_type == "RCC"
+    assert fluka_cylinder.uuid == "ed1507a5-0489-4dc3-bb87-7b27dcff43e7"
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_cylinder.coordinates,
+                                                                (2.0244531744545524, 1.4877257848751275, -0.45663660846935195)))
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_cylinder.height_vector,
+                                                                (0.23109365, -0.33545157, 0.91327322)))
+    assert fluka_cylinder.radius == 1
 
 def test_parse_sphere():
-    figure = solid_figures.SphereFigure(uuid='ed1507a5-0489-4dc3-bb87-7b27dcff43e7',
+    sphere = solid_figures.SphereFigure(uuid='ed1507a5-0489-4dc3-bb87-7b27dcff43e7',
                                         name='Sphere',
                                         position=(2.14, 1.32, 0),
                                         radius=1)
-    logging.warn(parse_sphere(figure))
+
+    fluka_sphere = parse_sphere(sphere)
+
+    assert fluka_sphere.figure_type == "SPH"
+    assert fluka_sphere.uuid == "ed1507a5-0489-4dc3-bb87-7b27dcff43e7"
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_sphere.coordinates,
+                                                                (2.14, 1.32, 0)))
+    assert fluka_sphere.radius == 1
 
 def test_parse_box():
     figure = solid_figures.BoxFigure(uuid='ed1507a5-0489-4dc3-bb87-7b27dcff43e7',
@@ -54,4 +45,16 @@ def test_parse_box():
                                      x_edge_length=1,
                                      y_edge_length=1,
                                      z_edge_length=1)
-    logging.warn(parse_box(figure))
+
+    fluka_box = parse_box(figure)
+
+    assert fluka_box.figure_type == "BOX"
+    assert fluka_box.uuid == "ed1507a5-0489-4dc3-bb87-7b27dcff43e7"
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_box.coordinates,
+                                                                (1.65527732505, 1.32, 0.1226536929)))
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_box.x_vector,
+                                                                (0.96944534989, 6.9388939e-18, -0.24530738587)))
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_box.y_vector,
+                                                                (0.08228874766, 0.94205745278, 0.32520196440)))
+    assert all(math.isclose(a, b, abs_tol=1e-8) for a, b in zip(fluka_box.z_vector,
+                                                                (0.23109365109, -0.3354515697, 0.91327321693)))
