d757522dc0cf218590a9977c6a04571bdbe878a4
Bartosz Czop <brt0sh0n@gmail.com>
2023-11-11 11:39:46 +0100

    Base class Card changes

--- converter/fluka/cards/card.py
+++ converter/fluka/cards/card.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
+from converter.common import format_float
 
 
 @dataclass
 class Card:
     """Class representing one line (card) in Fluka input."""
 
-    tag: str = ""
+    codewd: str = ""
     what: list[str] = field(default_factory=list)
     sdum: str = ""
 
@@ -16,13 +17,18 @@ class Card:
         self.what += [""] * (6 - len(self.what))
 
     def __str__(self) -> str:
-        """Return the card as a string."""
-        line = f"{self.tag:<10}"
+        """Return card as string."""
+        return self._format_line()
+    
+    def _format_line(self) -> str:
+        """Return formatted line from given parameters."""
+        line = f"{self.codewd:<10}"
         for w in self.what:
             try:
                 num = float(w)
-                line += f"{num:>10.3E}" if len(str(w)) > 10 else f"{num:>10}"
+                num = format_float(num, 10)
+                line += f"{num:>10}"
             except ValueError:
                 line += f"{w:>10}"
-        line += f"{self.sdum:<10}"
+        line += f"{self.sdum:>10}"
         return line
