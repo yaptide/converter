2f9b43f081768320880ac0f91e0228027f2f2a0c
Bartosz Czop <brt0sh0n@gmail.com>
2023-11-13 18:28:28 +0100

    Deepsource fix

--- converter/fluka/cards/assignmat_card.py
+++ converter/fluka/cards/assignmat_card.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from converter.fluka.cards.card import Card
+
 
 @dataclass
 class AssignMatCard(Card):
@@ -10,7 +11,7 @@ class AssignMatCard(Card):
     - what (list[str]): list of parameters (see FLUKA manual for details)
     sdum is not used.
     """
-    
+
     codewd: str = "ASSIGNMA"
     region_name: str = ""
     material_name: str = "BLCKHOLE"
@@ -19,4 +20,4 @@ class AssignMatCard(Card):
         """Set material and region on appropriate positions in what list."""
         super().__post_init__()
         self.what[0] = self.material_name
-        self.what[1] = self.region_name 
+        self.what[1] = self.region_name
