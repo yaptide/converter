cd2088e51c68c23126c157ce7ff0a4baf4057ebc
kacper1409 <kacper1409@gmail.com>
2023-11-28 23:51:18 +0100

    #156 detector finished

--- converter/fluka/helper_parsers/detector_parser.py
+++ converter/fluka/helper_parsers/detector_parser.py
@@ -13,11 +13,32 @@ class Detector:
     z_max: float
     z_bins: int
 
-def parse_detector(detectors_json: dict, detectorUuid: str) -> USRBIN:
+def parse_detector(detectors_json: dict, detectorUuid: str) -> Detector:
 
     for detector in detectors_json['detectors']:
         if detector['uuid'] == detectorUuid:
-            return USRBIN(
-                name=detector['name'],
 
+            geometry_data = detector['geometryData']
+            x_min, y_min, z_min = geometry_data['position']
+            parameters = geometry_data['parameters']
+
+            x_max = x_min + parameters['depth']
+            y_max = y_min + parameters['height']
+            z_max = z_min + parameters['width']
+
+            x_bins = parameters['xSegments']
+            y_bins = parameters['ySegments']
+            z_bins = parameters['zSegments']
+
+            return Detector(
+                name=detector['name'],
+                x_min=x_min,
+                y_min=y_min,
+                z_min=z_min,
+                x_max=x_max,
+                y_max=y_max,
+                z_max=z_max,
+                x_bins=x_bins,
+                y_bins=y_bins,
+                z_bins=z_bins
             )
