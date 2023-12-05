faf4f7abd63a271d423ada7ffc85b01c21bdcf62
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-12-05 20:05:40 +0100

    Refactor Fluka scoring classes

--- converter/fluka/helper_parsers/scoring_parser.py
+++ converter/fluka/helper_parsers/scoring_parser.py
@@ -1,36 +1,73 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from converter.fluka.cards.card import Card
 
-from converter.fluka.helper_parsers.detector_parser import Detector, parse_detector
+from converter.fluka.helper_parsers.detector_parser import MeshDetector
 
 
 @dataclass
 class Scoring:
-    """Class representing Scoring"""
+    """Class representing single Scoring in Fluka"""
 
     detectorUuid: str
-    detector: Detector
-    name: str = ""
-    quantity: str = "DOSE"
+    name: str
+    output_unit: int
+    quantity: str
+
+    def __str__(self) -> str:
+        """Returns string representation of Scoring card"""
+        raise NotImplementedError('Scoring handler not implemented')
+
+
+@dataclass
+class UsrbinCartesianScoring(Scoring):
+    """Class representing USRBIN Cartesian Scoring in Fluka"""
+    detector: MeshDetector
+
+    def __str__(self) -> str:
+        """Returns string representation of USRBIN for cartesian scoring card"""
+        first_card = self._handle_first_card()
+        second_card = self._handle_second_card()
+
+        return f'{first_card!s}\n{second_card!s}\n'
+
+    def _handle_first_card(self) -> Card:
+        """Creates first Scoring card"""
+
+        # Probably we should add some kind of mapping here or when creating cards
+        particle_of_scoring = self.quantity
+        output_unit = str(self.output_unit * -1)
+
+        what = ['10.0', particle_of_scoring, output_unit, self.detector.x_max, self.detector.y_max, self.detector.z_max]
+
+        return Card(codewd='USRBIN', what=what, sdum='changeme')
+
+    def _handle_second_card(self) -> Card:
+        """Creates second Scoring card"""
+        what = [
+            self.detector.x_min,
+            self.detector.y_min,
+            self.detector.z_min,
+            self.detector.x_bins,
+            self.detector.y_bins,
+            self.detector.z_bins,
+        ]
+
+        return Card(codewd='USRBIN', what=what, sdum='&')
 
 
 def parse_scorings(detectors_json: dict, scorings_json: dict) -> list[Scoring]:
     """Creates list of Scorings from dictionaries"""
     scorings = []
     for scoring in scorings_json['outputs']:
-        detector = next(
-            (detector for detector in detectors_json['detectors'] if detector['uuid'] == scoring['detectorUuid']),
-            None
-        )
+        detector_dict = next(
+            (detector for detector in detectors_json['detectors'] if detector['uuid'] == scoring['detectorUuid']), None)
 
-        if detector['geometryData']['geometryType'] != "Mesh":
-            continue
-
-        scorings.append(
-            Scoring(
-                detectorUuid=scoring['detectorUuid'],
-                name=scoring['name'],
-                detector=parse_detector(detector)
-            )
-        )
+        if detector_dict and detector_dict['geometryData']['geometryType'] == 'Mesh':
+            scorings.append(
+                UsrbinCartesianScoring(detectorUuid=scoring['detectorUuid'],
+                                       output_unit=21,
+                                       quantity='DOSE',
+                                       name=scoring['name'],
+                                       detector=MeshDetector.parse_mesh_detector(detector_dict)))
 
     return scorings
--- tests/fluka/test_scoring_card.py
+++ tests/fluka/test_scoring_card.py
@@ -25,34 +25,38 @@ def detectors_json_2(project2_fluka_json: dict) -> dict:
 
 
 @pytest.fixture(scope='module')
-def expected_card() -> str:
+def expected_scores() -> str:
     """Returns expected Fluka scoring card sets"""
-    line_one = 'USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme'
-    line_two = 'USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&'
+    lines = [
+        'USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme',
+        'USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&'
+    ]
 
-    return f'{line_one}\n{line_two}\n'
+    return ''.join(f'{line}\n' for line in lines)
 
 
 @pytest.fixture(scope='module')
-def expected_card_2() -> str:
+def expected_scores_2() -> str:
     """Returns expected Fluka scoring card sets"""
-    line_one = 'USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme'
-    line_two = 'USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&'
-    line_three = 'USRBIN          10.0      DOSE     -22.0      12.5      25.0      37.5changeme'
-    line_four = 'USRBIN           7.5      15.0      22.5      10.0     100.0     150.0&'
+    lines = [
+        'USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme',
+        'USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&',
+        'USRBIN          10.0      DOSE     -22.0      12.5      25.0      37.5changeme',
+        'USRBIN           7.5      15.0      22.5      10.0     100.0     150.0&',
+    ]
 
-    return f'{line_one}\n{line_two}\n{line_three}\n{line_four}\n'
+    return ''.join(f'{line}\n' for line in lines)
 
 
-def test_scoring_card(detectors_json: dict, scorings_json: dict, expected_card: str) -> None:
+def test_scoring_card(detectors_json: dict, scorings_json: dict, expected_scores: str) -> None:
     scorings = parse_scorings(detectors_json, scorings_json)
     scorings_card = ScoringsCard(scorings)
 
-    assert scorings_card.__str__() == expected_card
+    assert str(scorings_card) == expected_scores
 
 
-def test_scoring_card_multiple_scorings(detectors_json_2: dict, scorings_json_2: dict, expected_card_2: str) -> None:
+def test_scoring_card_multiple_scorings(detectors_json_2: dict, scorings_json_2: dict, expected_scores_2: str) -> None:
     scorings = parse_scorings(detectors_json_2, scorings_json_2)
     scorings_card = ScoringsCard(scorings)
 
-    assert scorings_card.__str__() == expected_card_2
+    assert str(scorings_card) == expected_scores_2
