65db29b3318fe5350644eb8e89f040cbb538ff24
kacper1409 <kacper1409@gmail.com>
2023-12-05 18:12:55 +0100

    #156 added test with multiple scorings

--- tests/fluka/conftest.py
+++ tests/fluka/conftest.py
@@ -7,8 +7,22 @@ def project_fluka_path() -> Path:
     """Path to SHIELD-HIT12A project.json file"""
     return Path(__file__).parent / 'project.json'
 
+
 @pytest.fixture(scope='session')
 def project_fluka_json(project_fluka_path) -> dict:
     """Dictionary with project data for Fluka"""
     with open(project_fluka_path, 'r') as file_handle:
+        return json.load(file_handle)
+
+
+@pytest.fixture(scope='session')
+def project2_fluka_path() -> Path:
+    """Path to SHIELD-HIT12A project.json file"""
+    return Path(__file__).parent / 'project2.json'
+
+
+@pytest.fixture(scope='session')
+def project2_fluka_json(project2_fluka_path) -> dict:
+    """Dictionary with project data for Fluka"""
+    with open(project2_fluka_path, 'r') as file_handle:
         return json.load(file_handle)
