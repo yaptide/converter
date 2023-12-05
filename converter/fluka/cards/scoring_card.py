6df4a29d472e69d35d6fe46be6f053edb15333fe
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-12-05 19:22:23 +0100

    Multiple output units

--- converter/fluka/cards/scoring_card.py
+++ converter/fluka/cards/scoring_card.py
@@ -4,23 +4,18 @@ from converter.fluka.cards.card import Card
 from converter.fluka.helper_parsers.scoring_parser import Scoring
 
 
-def handle_first_card(scoring: Scoring) -> str:
+def handle_first_card(scoring: Scoring, output_unit: int = 21) -> str:
     """Creates first Scoring card"""
     # temporary assumption
-    binning_what = "10.0"
+    binning_what = '10.0'
     # DOSE according to:
     # https://flukafiles.web.cern.ch/manual/chapters/particle_and_material_codes/particles_codes.html
-    particle_of_scoring = "DOSE"
-    # Fixed for now
-    output_unit = '-21'
+    particle_of_scoring = 'DOSE'
+    output_unit = str(output_unit * -1)
 
-    first_card = Card(codewd="USRBIN")
+    first_card = Card(codewd='USRBIN')
     first_card.what = [
-        binning_what,
-        particle_of_scoring,
-        output_unit,
-        scoring.detector.x_max,
-        scoring.detector.y_max,
+        binning_what, particle_of_scoring, output_unit, scoring.detector.x_max, scoring.detector.y_max,
         scoring.detector.z_max
     ]
     first_card.sdum = 'changeme'
@@ -30,7 +25,7 @@ def handle_first_card(scoring: Scoring) -> str:
 
 def handle_second_card(scoring: Scoring) -> str:
     """Creates second Scoring card"""
-    second_card = Card(codewd="USRBIN")
+    second_card = Card(codewd='USRBIN')
     second_card.what = [
         scoring.detector.x_min,
         scoring.detector.y_min,
@@ -55,11 +50,12 @@ class ScoringsCard:
         # the second one is continuation of data included in first
 
         # temporary default for no symmetry
-        result = ""
+        result = ''
 
+        default_output_unit = 21
         for scoring in self.data:
-            first_card = handle_first_card(scoring)
+            first_card = handle_first_card(scoring, default_output_unit)
             second_card = handle_second_card(scoring)
-            result += f"{first_card}\n{second_card}\n"
-
+            result += f'{first_card}\n{second_card}\n'
+            default_output_unit += 1
         return result
--- tests/fluka/test_scoring_card.py
+++ tests/fluka/test_scoring_card.py
@@ -2,7 +2,6 @@ import pytest
 
 from converter.fluka.cards.scoring_card import ScoringsCard
 from converter.fluka.helper_parsers.scoring_parser import parse_scorings
-from tests.fluka.conftest import project2_fluka_json
 
 
 @pytest.fixture(scope='module')
@@ -16,33 +15,33 @@ def detectors_json(project_fluka_json) -> dict:
 
 
 @pytest.fixture(scope='module')
-def scorings_json_2(project2_fluka_json) -> dict:
+def scorings_json_2(project2_fluka_json: dict) -> dict:
     return project2_fluka_json['scoringManager']
 
 
 @pytest.fixture(scope='module')
-def detectors_json_2(project2_fluka_json) -> dict:
+def detectors_json_2(project2_fluka_json: dict) -> dict:
     return project2_fluka_json['detectorManager']
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def expected_card() -> str:
     """Returns expected Fluka scoring card sets"""
-    line_one = "USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme"
-    line_two = "USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&"
+    line_one = 'USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme'
+    line_two = 'USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&'
 
-    return f"{line_one}\n{line_two}\n"
+    return f'{line_one}\n{line_two}\n'
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def expected_card_2() -> str:
     """Returns expected Fluka scoring card sets"""
-    line_one   = "USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme"
-    line_two   = "USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&"
-    line_three = "USRBIN          10.0      DOSE     -21.0      12.5      25.0      37.5changeme"
-    line_four  = "USRBIN           7.5      15.0      22.5      10.0     100.0     150.0&"
+    line_one = 'USRBIN          10.0      DOSE     -21.0      0.05       5.0       6.0changeme'
+    line_two = 'USRBIN         -0.05      -5.0      -6.0       1.0     100.0     120.0&'
+    line_three = 'USRBIN          10.0      DOSE     -22.0      12.5      25.0      37.5changeme'
+    line_four = 'USRBIN           7.5      15.0      22.5      10.0     100.0     150.0&'
 
-    return f"{line_one}\n{line_two}\n{line_three}\n{line_four}\n"
+    return f'{line_one}\n{line_two}\n{line_three}\n{line_four}\n'
 
 
 def test_scoring_card(detectors_json: dict, scorings_json: dict, expected_card: str) -> None:
