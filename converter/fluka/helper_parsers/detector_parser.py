4591adfb1d8f41b169a1dacd6a47078686b2414f
kacper1409 <kacper1409@gmail.com>
2023-12-13 22:57:58 +0100

    added Cyl detector

--- converter/fluka/helper_parsers/detector_parser.py
+++ converter/fluka/helper_parsers/detector_parser.py
@@ -52,3 +52,57 @@ class MeshDetector:
     def __get_min_coord(center: float, size: float) -> float:
         """Returns minimal coordinate basing on center and size"""
         return center - size / 2
+
+
+@dataclass
+class CylinderDetector:
+    """Class representing detector with in shape of cylinder"""
+
+    name: str
+    x: float
+    y: float
+    r_min: float
+    r_max: float
+    z_min: float
+    z_max: float
+    r_bins: int
+    z_bins: int
+    phi_bins: int
+
+    @classmethod
+    def parse_cylinder_detector(cls, detector_dict: dict) -> 'CylinderDetector':
+        """Creates detector from dictionary"""
+        geometry_data = detector_dict['geometryData']
+        parameters = geometry_data['parameters']
+
+        x = geometry_data['position'][0]
+        y = geometry_data['position'][1]
+
+        r_min = parameters['innerRadius']
+        r_max = parameters['radius']
+
+        depth = parameters['depth']
+        z_min = cls.__get_min_coord(geometry_data['position'][2], depth)
+        z_max = z_min + depth
+
+        r_bins = parameters['radialSegments']
+        z_bins = parameters['zSegments']
+
+        # default from fluka documentation, not provided in json dict
+        phi_bins = 1
+
+        return CylinderDetector(name=detector_dict['name'],
+                                x=x,
+                                y=y,
+                                r_min=r_min,
+                                r_max=r_max,
+                                z_min=z_min,
+                                z_max=z_max,
+                                r_bins=r_bins,
+                                z_bins=z_bins,
+                                phi_bins=phi_bins
+        )
+
+    def __get_min_coord(center: float, size: float) -> float:
+        """Returns minimal coordinate basing on center and size"""
+        return center - size / 2
