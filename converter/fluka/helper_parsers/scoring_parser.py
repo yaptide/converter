e20323f86b988a0363015e5524b06a608a7548fb
kacper1409 <kacper1409@gmail.com>
2023-12-18 21:50:17 +0100

    added scoring support for CylinderDetector

--- converter/fluka/helper_parsers/scoring_parser.py
+++ converter/fluka/helper_parsers/scoring_parser.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from typing import Union
 
 from converter.fluka.cards.card import Card
-from converter.fluka.helper_parsers.detector_parser import MeshDetector, parse_mesh_detector
+from converter.fluka.helper_parsers.detector_parser import MeshDetector, parse_mesh_detector, CylinderDetector
 
 
 @dataclass
@@ -22,7 +23,7 @@ class Scoring:
 class UsrbinCartesianScoring(Scoring):
     """Class representing USRBIN Cartesian Scoring in Fluka"""
 
-    detector: MeshDetector
+    detector: Union[MeshDetector, CylinderDetector]
 
     def __str__(self) -> str:
         """Returns string representation of USRBIN for cartesian scoring card"""
@@ -36,21 +37,48 @@ class UsrbinCartesianScoring(Scoring):
         # Probably we should add some kind of mapping here or when creating cards
         particle_of_scoring = self.quantity
         output_unit = str(self.output_unit * -1)
-
-        what = ['10.0', particle_of_scoring, output_unit, self.detector.x_max, self.detector.y_max, self.detector.z_max]
+        if isinstance(self.detector, MeshDetector):
+            what = [
+                '10.0',
+                particle_of_scoring,
+                output_unit,
+                self.detector.x_max,
+                self.detector.y_max,
+                self.detector.z_max
+            ]
+        elif isinstance(self.detector, CylinderDetector):
+            what = [
+                '10.0',
+                particle_of_scoring,
+                output_unit,
+                self.detector.r_max,
+                self.detector.y,
+                self.detector.z_max
+            ]
 
         return Card(codewd='USRBIN', what=what, sdum='changeme')
 
     def _handle_second_card(self) -> Card:
         """Creates second Scoring card"""
-        what = [
-            self.detector.x_min,
-            self.detector.y_min,
-            self.detector.z_min,
-            self.detector.x_bins,
-            self.detector.y_bins,
-            self.detector.z_bins,
-        ]
+
+        if isinstance(self.detector, CylinderDetector):
+            what = [
+                self.detector.r_min,
+                self.detector.x,
+                self.detector.z_min,
+                self.detector.r_min,
+                self.detector.phi_bins,
+                self.detector.z_bins,
+            ]
+        else:
+            what = [
+                self.detector.x_min,
+                self.detector.y_min,
+                self.detector.z_min,
+                self.detector.x_bins,
+                self.detector.y_bins,
+                self.detector.z_bins,
+            ]
 
         return Card(codewd='USRBIN', what=what, sdum='&')
 
