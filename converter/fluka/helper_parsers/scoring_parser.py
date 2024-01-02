839b3f76b0844dac60c5c07ae4f7d8d358bfd3b8
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2024-01-02 17:02:23 +0100

    Comment old implementation

--- converter/fluka/helper_parsers/scoring_parser.py
+++ converter/fluka/helper_parsers/scoring_parser.py
@@ -9,28 +9,28 @@ class Scoring:
 
     detectorUuid: str
     detector: Detector
-    name: str = ""
-    quantity: str = "DOSE"
+    name: str = ''
+    quantity: str = 'DOSE'
 
 
-def parse_scorings(detectors_json: dict, scorings_json: dict) -> list[Scoring]:
-    """Creates list of Scorings from dictionaries"""
-    scorings = []
-    for scoring in scorings_json['outputs']:
-        detector = next(
-            (detector for detector in detectors_json['detectors'] if detector['uuid'] == scoring['detectorUuid']),
-            None
-        )
+# def parse_scorings(detectors_json: dict, scorings_json: dict) -> list[Scoring]:
+#     """Creates list of Scorings from dictionaries"""
+#     scorings = []
+#     for scoring in scorings_json['outputs']:
+#         detector = next(
+#             (detector for detector in detectors_json['detectors'] if detector['uuid'] == scoring['detectorUuid']),
+#             None
+#         )
 
-        if detector['geometryData']['geometryType'] != "Mesh":
-            continue
+#         if detector['geometryData']['geometryType'] != "Mesh":
+#             continue
 
-        scorings.append(
-            Scoring(
-                detectorUuid=scoring['detectorUuid'],
-                name=scoring['name'],
-                detector=parse_detector(detector)
-            )
-        )
+#         scorings.append(
+#             Scoring(
+#                 detectorUuid=scoring['detectorUuid'],
+#                 name=scoring['name'],
+#                 detector=parse_detector(detector)
+#             )
+#         )
 
-    return scorings
+#     return scorings
