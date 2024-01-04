026c4863a199fade056be28311c4dcf952f7e42f
kacper1409 <kacper1409@gmail.com>
2024-01-04 21:49:44 +0100

    input revert

--- converter/fluka/input.py
+++ converter/fluka/input.py
@@ -47,7 +47,30 @@ GEOEND
 {COMPOUNDS}
 {MATPROPS}
 {ASSIGNMATS}
-{SCORINGS}
+* scoring NEUTRON on mesh z
+USRBIN           0.0   NEUTRON       -21       0.5       0.5       5.0n_z
+USRBIN          -0.5      -0.5       0.0         1         1       500&
+* scoring NEUTRON on mesh yz
+USRBIN           0.0   NEUTRON       -22       0.1       5.0       5.0n_yz
+USRBIN          -0.1      -5.0       0.0         1       500       500&
+* scoring NEUTRON on mesh xy
+USRBIN           0.0   NEUTRON       -23       5.0       5.0       2.9n_xy
+USRBIN          -5.0      -5.0       2.8       500       500         1&
+* scoring NEUTRON on mesh zx
+USRBIN           0.0   NEUTRON       -24       5.0       0.1       5.0n_zx
+USRBIN          -5.0      -0.1       0.0       500         1       500&
+* scoring ENERGY on mesh z
+USRBIN           0.0    ENERGY       -25       0.5       0.5       5.0en_z
+USRBIN          -0.5      -0.5       0.0         1         1       500&
+* scoring ENERGY on mesh yz
+USRBIN           0.0    ENERGY       -26       0.1       5.0       5.0en_yz
+USRBIN          -0.1      -5.0       0.0         1       500       500&
+* scoring ENERGY on mesh xy
+USRBIN           0.0    ENERGY       -27       5.0       5.0       2.9en_xy
+USRBIN          -5.0      -5.0       2.8       500       500         1&
+* scoring ENERGY on mesh zx
+USRBIN           0.0    ENERGY       -28       5.0       0.1       5.0en_zx
+USRBIN          -5.0      -0.1       0.0       500         1       500&
 * random number generator settings
 RANDOMIZ                   137
 * number of particles to simulate
