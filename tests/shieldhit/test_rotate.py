f310e36b4794276e7d8c3735ab64d374b2b7bb2b
dudiiiiiiii <dudekb@student.agh.edu.pl>
2024-04-10 17:18:43 +0200

    more test cases added

--- tests/shieldhit/test_rotate.py
+++ tests/shieldhit/test_rotate.py
@@ -3,13 +3,11 @@ from scipy.spatial.transform import Rotation
 import pytest
 
 
-@pytest.mark.parametrize(
-    'vector,angles',
-    [
-        ([1, 0, 0], [90, 0, 0]),
-        ([0, 1, 0], [270, 0, 0]),
-        # Add more test cases here
-    ])
+@pytest.mark.parametrize('vector,angles', [([1, 0, 0], [90, 0, 0]), ([0, 1, 0], [270, 0, 0]), ([1, 0, 0], [90, 90, 90]),
+                                           ([0, 0, 0], [45, 45, 45]), ([2, 3, 4], [30, 45, 60]),
+                                           ([1, 1, 1], [-45, -30, -60]), ([1, 1, 1], [270, 180, 360]),
+                                           ([-1, -2, -3], [45, 45, 45]), ([0, 1, 1], [45, 0, 0]),
+                                           ([2, 4, -7], [23, -82, 213])])
 def test_rotate(vector, angles, precision=8):
     """Test the rotate function with various inputs. Expects angles as degrees"""
     # Rotate using custom function
