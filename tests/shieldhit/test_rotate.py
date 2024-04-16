40d830d667cd03bb56f8f1e5d236b48e4f173383
dudiiiiiiii <dudekb@student.agh.edu.pl>
2024-04-16 12:31:21 +0200

    add more tests

--- tests/shieldhit/test_rotate.py
+++ tests/shieldhit/test_rotate.py
@@ -1,20 +1,31 @@
 from converter.common import rotate, format_float
 from scipy.spatial.transform import Rotation
+import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize('vector,angles', [([1, 0, 0], [90, 0, 0]), ([0, 1, 0], [270, 0, 0]), ([1, 0, 0], [90, 90, 90]),
-                                           ([0, 0, 0], [45, 45, 45]), ([2, 3, 4], [30, 45, 60]),
-                                           ([1, 1, 1], [-45, -30, -60]), ([1, 1, 1], [270, 180, 360]),
-                                           ([-1, -2, -3], [45, 45, 45]), ([0, 1, 1], [45, 0, 0]),
-                                           ([2, 4, -7], [23, -82, 213])])
-def test_rotate(vector: list[float], angles: list[float]):
-    """Test the rotate function with various inputs. Expects angles as degrees"""
+@pytest.mark.parametrize('vector,angles,degrees', [([1, 0, 0], [90, 0, 0], True), ([0, 1, 0], [270, 0, 0], True),
+                                                   ([1, 0, 0], [90, 90, 90], True), ([0, 0, 0], [45, 45, 45], True),
+                                                   ([2, 3, 4], [30, 45, 60], True), ([1, 1, 1], [-45, -30, -60], True),
+                                                   ([1, 1, 1], [270, 180, 360], True),
+                                                   ([-1, -2, -3], [45, 45, 45], True), ([0, 1, 1], [45, 0, 0], True),
+                                                   ([2, 4, -7], [23, -82, 213], True), ([1, 0, 0], [0, 0, 0], True),
+                                                   ([0, 1, 0], [0, 0, 0], True), ([0, 0, 1], [0, 0, 0], True),
+                                                   ([1, 0, 0], [np.pi / 2, 0, 0], False),
+                                                   ([0, 1, 0], [np.pi / 2, 0, 0], False),
+                                                   ([0, 0, 1], [np.pi / 2, 0, 0], False),
+                                                   ([0, 1, 0], [0, np.pi / 2, 0], False),
+                                                   ([0, 0, 1], [0, 0, np.pi / 2], False),
+                                                   ([2, 4, -1], [np.pi / 4, np.pi / 3, np.pi / 2], False),
+                                                   ([0, 0, 0], [45, 45, 45], True), ([0, 0, 1], [90, 0, 0], True),
+                                                   ([0, 0, 1], [0, 0, 90], True), ([0, 0, 1], [0, 0, 180], True)])
+def test_rotate(vector: list[float], angles: list[float], degrees: bool):
+    """Test the rotate function with various inputs."""
     # Rotate using custom function
-    result = rotate(vector, angles)
+    result = rotate(vector, angles, degrees)
 
     # Rotate using SciPy
-    r = Rotation.from_euler('xyz', angles, degrees=True)
+    r = Rotation.from_euler('xyz', angles, degrees=degrees)
     result_scipy = r.apply(vector)
 
     assert pytest.approx(result) == result_scipy
