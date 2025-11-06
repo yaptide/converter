d15f380d240122a298220043b504122e0f9e1213
Konrad Michalik <kmichalikk19@gmail.com>
2025-11-06 23:56:42 +0100

    Fix fluka tests

--- tests/fluka/test_fluka_beam.py
+++ tests/fluka/test_fluka_beam.py
@@ -20,16 +20,49 @@ def test_parse_fluka_beam(project_fluka_json):
     assert isclose(fluka_beam.beam_dir[1], 0.0, abs_tol=1e-16)
 
 
-def test_parse_heavy_ions(project_fluka_json):
+def test_parse_fluka_beam_MeV_nucl(project_fluka_json):
     """Test if Fluka beam is parsed correctly"""
     beam_json = copy.deepcopy(project_fluka_json['beam'])
+    beam_json['energyUnit'] = 'MeV/nucl'
+    beam_json['energy'] = 100
+    beam_json['particle']['id'] = 23
+    beam_json['particle']['a'] = 3
+
+    fluka_beam = parse_beam(beam_json)
+
+    assert fluka_beam.energy_MeV == 300
+    assert fluka_beam.particle_name == '3-HELIUM'
+
+
+def test_parse_heavy_ions_MeV(project_fluka_json):
+    """Test if Fluka beam is parsed correctly"""
+    beam_json = copy.deepcopy(project_fluka_json['beam'])
+    beam_json['energy'] = 300
+    beam_json['energyUnit'] = 'MeV'
     beam_json['particle']['id'] = 25
     beam_json['particle']['a'] = 6
     beam_json['particle']['z'] = 12
 
     fluka_beam = parse_beam(beam_json)
 
-    assert fluka_beam.energy_MeV == 70
+    assert fluka_beam.energy_MeV == 50
+    assert fluka_beam.particle_name == 'HEAVYION'
+    assert fluka_beam.heavy_ion_a == 6
+    assert fluka_beam.heavy_ion_z == 12
+
+
+def test_parse_heavy_ions_MeV_nucl(project_fluka_json):
+    """Test if Fluka beam is parsed correctly"""
+    beam_json = copy.deepcopy(project_fluka_json['beam'])
+    beam_json['energy'] = 300
+    beam_json['energyUnit'] = 'MeV/nucl'
+    beam_json['particle']['id'] = 25
+    beam_json['particle']['a'] = 6
+    beam_json['particle']['z'] = 12
+
+    fluka_beam = parse_beam(beam_json)
+
+    assert fluka_beam.energy_MeV == 300
     assert fluka_beam.particle_name == 'HEAVYION'
     assert fluka_beam.heavy_ion_a == 6
     assert fluka_beam.heavy_ion_z == 12
